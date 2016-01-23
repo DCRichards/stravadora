0a1cd03267be6e960e1737e4e611484080aa0102
DCRichards <DCRichards@live.co.uk>
2016-01-23 15:01:40 +0000

    additional javadoc

--- app/src/main/java/com/dcrichards/stravadora/StravaCallback.java
+++ app/src/main/java/com/dcrichards/stravadora/StravaCallback.java
@@ -7,8 +7,18 @@ package com.dcrichards.stravadora;
  */
 public interface StravaCallback<T> {
 
+    /**
+     * On result returned from action
+     *
+     * @param result Result of specified type
+     */
     void onResult(T result);
 
+    /**
+     * On error returned from action
+     *
+     * @param ex The exception thrown
+     */
     void onError(Exception ex);
 
 }
